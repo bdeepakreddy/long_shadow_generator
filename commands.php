241f7c19864628b87ca0c237531d2cef25b13f42
sudeep <sudipmelepunathilmp@gmail.com>
2014-10-28 11:40:51 +0530

    commands update

--- commands.php
+++ commands.php
@@ -1,8 +1,14 @@
 <?php
 // if POST request
 // if($_POST) {
-	$output = shell_exec('ls');
+	$output = shell_exec('fontcustom compile');
+	if($output) {
+		$path = 'generated/fonts/';
+	}
+	// #TODO create a unique folder name
+																							
 	var_dump($output);
+	var_dump($path);
 // }
 
 ?>
